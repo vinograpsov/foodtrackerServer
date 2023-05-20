4edd276ca1c3a1f1c4b66440f3c669d2515ddcac
vinograpsov <kirvinpost@gmail.com>
2023-05-20 20:44:16 +0200

    full working connection to database

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
+from models import db, Users, Elements, Fridge, Products, Recipes, RecipesProducts
+
+def get_db_secrets():
+    with open('./secrets.txt', 'r') as f:
+        username = f.readline().strip()
+        password = f.readline().strip() 
+    return username, password
+
+app = Flask(__name__)
+username, password = get_db_secrets()
+database_server_url = 'food-app-server.mysql.database.azure.com'
+database_name = 'foodtracker'
+cert_path = './certs/DigiCertGlobalRootCA.crt.pem'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{username}:{password}@{database_server_url}/{database_name}"
+    f"?ssl_ca={cert_path}"
+)
+
+db.init_app(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all();
+        try:
+            users = Users.query.all()
+            for user in users:
+                print(f"User ID: {user.id}, Username: {user.username}")
+
+            elements = Elements.query.all()
+            for element in elements:
+                print(f"User ID: {element.user_id}, Date: {element.date_usr}")
+
+            fridges = Fridge.query.all()
+            for fridge in fridges:
+                print(f"User ID: {fridge.user_id}, Product ID: {fridge.product_id}")
+
+            products = Products.query.all()
+            for product in products:
+                print(f"Product ID: {product.id}, Product Name: {product.product_name}")
+
+            recipes = Recipes.query.all()
+            for recipe in recipes:
+                print(f"Recipe ID: {recipe.id}, Recipe Name: {recipe.name}")
+
+            recipes_products = RecipesProducts.query.all()
+            for rp in recipes_products:
+                print(f"Product ID: {rp.product_id}, Recipe ID: {rp.recipe_id}")
+        except Exception as e:
+            print("connection to database failed")
+            print(e)    
+
---
+++ models.py
@@ -0,0 +1,74 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    usr_password = db.Column(db.String(255), nullable=False)
+    age = db.Column(db.Integer, nullable = False, default  = 18)
+    weight = db.Column(db.Float, nullable = False, default = 62.0)
+    height = db.Column(db.Float, nullable = False, default = 175.0)
+    sex = db.Column(db.String(6), nullable = False)
+    activity_level = db.Column(db.String(100), nullable = True)
+    img_url = db.Column(db.String(100), nullable = True)
+
+
+class Elements(db.Model):
+    __tablename__ = 'elements'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    date_usr = db.Column(db.Date, primary_key=True)
+    calories = db.Column(db.Float, nullable=False)
+    protein = db.Column(db.Float, nullable=False)
+    fat = db.Column(db.Float, nullable=False)
+    carbohydrates = db.Column(db.Float)
+
+    # user = db.relationship('Users', backref=db.backref('elements', lazy=True))
+
+
+class Fridge(db.Model):
+    __tablename__ = 'fridge'
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    location = db.Column(db.String(50), nullable=False)
+    expire_data = db.Column(db.Date, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    how_much = db.Column(db.Float, nullable=False)
+
+    # user = db.relationship('Users', backref=db.backref('fridge', lazy=True))
+    # product = db.relationship('Products', backref=db.backref('fridge', lazy=True))
+
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    product_name = db.Column(db.String(255), nullable=False)
+    calories = db.Column(db.Float, nullable=False)
+    protein = db.Column(db.Float, nullable=False)
+    fat = db.Column(db.Float, nullable=False)
+    carbohydrates = db.Column(db.Float)
+
+    # user = db.relationship('Users', backref=db.backref('products', lazy=True))
+
+
+class Recipes(db.Model):
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(255), nullable=False)
+    annotation = db.Column(db.String(255), nullable=False)
+    recepe_text = db.Column(db.String(10000), nullable=False)
+    rate = db.Column(db.String(1), nullable=False)
+
+    # user = db.relationship('Users', backref=db.backref('recipes', lazy=True))
+
+
+class RecipesProducts(db.Model):
+    __tablename__ = 'recipes_products'
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
+
+    # product = db.relationship('Users', backref=db.backref('recipes_products', lazy=True))
+    # recipe = db.relationship('Products', backref=db.backref('recipes_products', lazy=True))
