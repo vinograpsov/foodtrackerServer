1a94522568976a430174a57a8c1a01f8e5270695
vinograpsov <kirvinpost@gmail.com>
2023-05-21 11:56:27 +0200

    tested endpoint templates

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from sqlalchemy import text
 
 from models import db, Users, Elements, Fridge, Products, Recipes, RecipesProducts
 
+
+# ----------------- DATABASE CONNECTION-----------------
 def get_db_secrets():
     with open('./secrets.txt', 'r') as f:
         username = f.readline().strip()
@@ -20,8 +22,65 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://{username}:{password}@{database_server_url}/{database_name}"
     f"?ssl_ca={cert_path}"
 )
-
 db.init_app(app)
+# ----------------- DATABASE CONNECTION-----------------
+
+
+# -----------------  ADD VIEWS -----------------
+@app.route("/auth/me",methods=["GET"])
+def h():
+    return "Auth me"
+
+@app.route("/auth/signin",methods=["POST"])
+def signin():
+    return "Sign in"
+
+@app.route("/auth/signup",methods=["POST"])
+def signup():
+    return "Sign up"
+
+@app.route("/calories",methods=["GET"])
+def calories_get():
+    return "Calories"
+
+@app.route("/calories",methods=["POST"])
+def calories_post():
+    return "Calories"
+
+@app.route("/fridge/bymyself",methods=["POST"])
+def fridge_bymyself():
+    return "Fridge by myself"
+
+@app.route("/fridge/byqrcode", methods=["POST"])
+def fridge_byqrcode():
+    return "Fridge by QR code"
+
+@app.route("/fridge/<int:product_id>", methods=["DELETE"])
+def fridge_delete(product_id):
+    return "Fridge delete"
+
+@app.route("/fridge/<int:product_id>", methods=["PATCH"])
+def fridge_patch(product_id):  
+    return "Fridge patch"
+
+@app.route("/recepes/my", methods = ["GET"])
+def recepes_my():
+    return "Recepes my"
+
+@app.route("/recepes/global", methods = ["POST"])
+def recepes_global():
+    return "Recepes global post"
+
+@app.route("/recepes/global", methods = ["GET"])
+def recepes_global_get():
+    return "Recepes global get"
+
+@app.route("/settings", methods = ["PATCH"])
+def settings_patch():
+    return "Settings patch"
+# -----------------  ADD VIEWS -----------------
+
+
 
 if __name__ == "__main__":
     with app.app_context():
---
+++ views.py
@@ -0,0 +1,6 @@
+from flask import request
+from flask.views import MethodView  
+from models import db, Users, Elements, Fridge, Products, Recipes, RecipesProducts
+
+
+
